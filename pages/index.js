e648e0e9c593472ab6e15a8ed818aa0e187c2423
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-10-04 07:26:17 -0500

    Workaround weird chrome thing

--- pages/index.js
+++ pages/index.js
@@ -49,6 +49,7 @@ export default class extends React.Component {
         body {
           background-image: url('/static/va-bg.jpg');
           background-size: cover;
+          background-position: bottom;
         }
 
         .container {
