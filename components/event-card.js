380b965e2087cc22ad13152060e6f570840c2c63
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-10-11 21:55:28 -0500

    Cleaner

--- components/event-card.js
+++ components/event-card.js
@@ -1,25 +1,25 @@
-export default ({ event: event }) => {
-    let color = "rgba(" + parseInt(255 * Math.random()) + "," + parseInt(255 * Math.random()) + "," + parseInt(255 * Math.random()) + ",0.5)";
-    return (<ul className="event-card" style={{ backgroundColor: color }}>
-        <style jsx>{`
-        .event-card {
-            background-color: rgba(0,0,0,0.1);
-            border-radius: 4px;
-            list-style-type: none;
-            padding: 1.5em;
-        }
-
-        .event-title {
-            font-size: 20px;
-            font-weight: bold;
-        }
-
-        .event-date {
-            font-size: 16px;
-        }
-        `}</style>
-        <li className="event-title">{event.name}</li>
-        <li className="event-date">{event.date}</li>
-        <li>{event.descr.map((line, i) => <p key={i}>{line}</p>)}</li>
-    </ul>)
+function generateColor(seed) {
+    return `rgba(${parseInt(255 * Math.random())}, ${parseInt(255 * Math.random())}, ${parseInt(255 * Math.random())}, 0.5)`;
 }
+
+export default ({ event: event }) => <ul className="event-card" style={{ backgroundColor: generateColor() }}>
+    <style jsx>{`
+    .event-card {
+        border-radius: 4px;
+        list-style-type: none;
+        padding: 1.5em;
+    }
+
+    .event-title {
+        font-size: 20px;
+        font-weight: bold;
+    }
+
+    .event-date {
+        font-size: 16px;
+    }
+    `}</style>
+    <li className="event-title">{event.name}</li>
+    <li className="event-date">{event.date}</li>
+    <li>{event.descr.map((line, i) => <p key={i}>{line}</p>)}</li>
+</ul>
