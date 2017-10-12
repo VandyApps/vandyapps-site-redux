303f7a636c4c0fc42a5981b88acbb2fa8544de6d
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-10-12 16:21:39 -0500

    Use div

--- components/event-card.js
+++ components/event-card.js
@@ -4,7 +4,6 @@ export default ({ event: event, eventKey: key }) => <ul className="event-card" s
     <style jsx>{`
     .event-card {
         border-radius: 4px;
-        list-style-type: none;
         padding: 1.5em;
         margin: 2.5em 0;
         box-shadow: inset 0 0 0 .5px rgba(0,0,0,0.125), 0 12.5px 30px rgba(0,0,0,0.07);
@@ -20,7 +19,7 @@ export default ({ event: event, eventKey: key }) => <ul className="event-card" s
         font-weight: bold;
     }
     `}</style>
-    <li className="event-date">{event.date}</li>
-    <li className="event-title">{event.name}</li>
-    <li>{event.descr.map((line, i) => <p key={i}>{line}</p>)}</li>
+    <div className="event-date">{event.date}</div>
+    <div className="event-title">{event.name}</div>
+    <div>{event.descr.map((line, i) => <p key={i}>{line}</p>)}</div>
 </ul>
