785d1a1184fee698cdcd499de3d02281c0ca2e71
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-10-11 21:51:59 -0500

    Cleaner markup

--- components/event-card.js
+++ components/event-card.js
@@ -1,13 +1,25 @@
-export default props => {
-    const event = props.event;
+export default ({ event: event }) => {
     let color = "rgba(" + parseInt(255 * Math.random()) + "," + parseInt(255 * Math.random()) + "," + parseInt(255 * Math.random()) + ",0.5)";
-    return (<ul className="eventBox" style={{ backgroundColor: color }}>
-        <li className="eventTitle">{event.name}</li>
-        <li className="eventDate">{event.date}</li>
-        <li>{
-            event.descr.map((lineOfText, i) => {
-                return (<p key={i}>{lineOfText}</p>)
-            })
-        }</li>
+    return (<ul className="event-card" style={{ backgroundColor: color }}>
+        <style jsx>{`
+        .event-card {
+            background-color: rgba(0,0,0,0.1);
+            border-radius: 4px;
+            list-style-type: none;
+            padding: 1.5em;
+        }
+
+        .event-title {
+            font-size: 20px;
+            font-weight: bold;
+        }
+
+        .event-date {
+            font-size: 16px;
+        }
+        `}</style>
+        <li className="event-title">{event.name}</li>
+        <li className="event-date">{event.date}</li>
+        <li>{event.descr.map((line, i) => <p key={i}>{line}</p>)}</li>
     </ul>)
 }
--- pages/schedule.js
+++ pages/schedule.js
@@ -1,6 +1,7 @@
 import Meta from '../layouts/meta';
 import Header from '../components/header';
-import Events from '../components/events';
+import EventCard from '../components/event-card';
+import eventData from '../resources/events.json'
 
 export default () =>
     <div className='container content-page schedule'>
@@ -10,6 +11,8 @@ export default () =>
             <h1>Schedule</h1>
             <p>This page will list the events VandyApps will hold throughout the year. It's also
 			being regularly updated so please be sure to check back often!</p>
-            <Events />
+            <div className="event-list">
+            {eventData.events.map((event, i) => <EventCard key={i} event={event} />)}
+            </div>
         </div>
     </div>
