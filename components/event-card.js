e0902f9866880ebbfb7ea097c8f9f199a9b0be07
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-11-04 11:51:33 -0500

    Event card padding tweaks

--- components/event-card.js
+++ components/event-card.js
@@ -3,8 +3,14 @@ import randomColor from '../resources/random-color'
 export default ({ event: event, eventKey: key }) => <div className="event-card shadow-layer" style={{ borderTop: `4px solid ${randomColor(key)}` }}>
     <style jsx>{`
     .event-card {
-        padding: 1.5em;
+        padding: 1.25em 1.25em .75em;
         margin: 2.5em 0;
+    }
+
+    @media (min-width: 680px) {
+        .event-card {
+            padding: 1.5em 1.5em 1em;
+        }
     }
 
     .event-date {
