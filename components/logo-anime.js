ca211cc54275c5e8f10be9328bc95516d11f32f9
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2018-01-25 08:09:47 -0600

    Reformat

--- components/logo-anime.js
+++ components/logo-anime.js
@@ -3,16 +3,16 @@ import Anime from 'react-anime';
 const setVisible = ({ animatables: [{ target }] }) =>
     target.setAttribute('visibility', 'visible');
 
+const animSharedAttrs = {
+    duration: 150,
+    begin: setVisible,
+    easing: 'easeOutSine'
+};
+
 export default function(props) {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 10 456 326.5">
-            <Anime
-                width={200}
-                delay={650}
-                duration={150}
-                begin={setVisible}
-                easing="easeOutSine"
-            >
+            <Anime width={200} delay={650} {...animSharedAttrs}>
                 <rect
                     fill="#5d5b66"
                     visibility="hidden"
@@ -24,13 +24,7 @@ export default function(props) {
                     ry="36.5"
                 />
             </Anime>
-            <Anime
-                width={349}
-                delay={200}
-                duration={150}
-                begin={setVisible}
-                easing="easeOutSine"
-            >
+            <Anime width={349} delay={200} {...animSharedAttrs}>
                 <rect
                     fill="#feb249"
                     visibility="hidden"
@@ -43,13 +37,7 @@ export default function(props) {
                     transform="rotate(64.5 108.069 173.215)"
                 />
             </Anime>
-            <Anime
-                width={347.5}
-                delay={500}
-                duration={150}
-                begin={setVisible}
-                easing="easeOutSine"
-            >
+            <Anime width={347.5} delay={500} {...animSharedAttrs}>
                 <rect
                     fill="#15c3c2"
                     visibility="hidden"
@@ -62,13 +50,7 @@ export default function(props) {
                     transform="rotate(64.5 345.869 173.215)"
                 />
             </Anime>
-            <Anime
-                width={349}
-                delay={350}
-                duration={150}
-                begin={setVisible}
-                easing="easeOutSine"
-            >
+            <Anime width={349} delay={350} {...animSharedAttrs} Z>
                 <rect
                     fill="#f05854"
                     visibility="hidden"
